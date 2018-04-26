fc9134636922807c97165086fc48bf2d958d385e
Tamás Németh <tomuwhu@gmail.com>
2018-04-26 13:46:13 +0200

    .

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,13 @@
   <div class="text-center">
     <br><br>
     <h2>
-      <a target="_blank" href="https://nuxtjs.org/">NuxtJS</a> -
+      [<a target="_blank" href="https://nuxtjs.org/">NuxtJS</a> -
       <a target="_blank" href="https://expressjs.com/">ExpressJS</a> -
       <a target="_blank" href="https://vuejs.org/v2/guide/">Vue.js</a> -
-      <a target="_blank" href="https://bootstrap-vue.js.org/">Bootstrap</a> -            
-      <a target="_blank" href="http://mongoosejs.com/">Mongoose mintaalkalmazás</a>
+      <a target="_blank" href="https://vuex.vuejs.org/en/intro.html">Vuex</a> -      
+      <a target="_blank" href="https://bootstrap-vue.js.org/">Bootstrap</a> -
+      <a target="_blank" href="http://mongoosejs.com/">Mongoose</a>]
+       mintaalkalmazás
     </h2>
     <br>
     <form v-if="!$store.state.authUser" @submit.prevent="login">
