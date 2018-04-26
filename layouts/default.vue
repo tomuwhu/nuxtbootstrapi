beef38479ff9a5ce49bc1a6e6c67c554c1993f07
Tamás Németh <tomuwhu@gmail.com>
2018-04-26 10:50:47 +0200

    .

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="info">
+     <b-navbar-nav>
+       <b-nav-item href="/">Kezdőlap</b-nav-item>
+       <b-nav-item href="/bs">Shopping list</b-nav-item>
+     </b-navbar-nav>
+    </b-navbar>
+    <nuxt/>
+  </div>
+</template>
